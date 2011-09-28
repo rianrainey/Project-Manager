a74a7b4b701c18c187e75b5150a7ae77a2b11c00
Rian Rainey <rian@rianrainey.com>
2011-09-28 19:48:26 -0400

    Added bi_proj_mgmt to git repo.

---
+++ bi_proj_mgmt/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BiProjMgmt::Application.config.secret_token = 'a14410be4ae18f76efa896354c79526294be4126e7040d40daa7cb23bdb8ca544c6edbdbf4ff1c5e9f83e00211cef693aa52558bb3efb311379bddbf9954fec3'
