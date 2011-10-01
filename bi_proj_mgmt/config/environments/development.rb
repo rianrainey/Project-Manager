1a6fd55ee76f7a45b3aa62e20d00e5cde0757d56
Rian Rainey <rian@rianrainey.com>
2011-10-01 01:20:29 -0400

    Added devise, compass, and rspec.

--- bi_proj_mgmt/config/environments/development.rb
+++ bi_proj_mgmt/config/environments/development.rb
@@ -22,5 +22,8 @@ BiProjMgmt::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  # From Devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
 
